a4dd24a9f041615c326ef96e578fb9d6de725392
jorge <jorgemartinez.jam@gmail.com>
2024-08-15 09:25:12 -0300

    FINAL

--- back/src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-// const moviesController =  (req, res) => {
-//     console.log("el controlador esta recibiendo una solicitud");
-//     res.status(200).send("Próximamente estarán disponibles los datos de películas.");
-// };
-
-// module.exports = {
-//     moviesController,
-// };
--- back/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-// const { Router } = require("express");
-
-// const moviesRouter = require("./moviesRouter.js");
-
-// const router = Router();
-
-// router.use("/movies", moviesRouter);
-
-// module.exports = router;
